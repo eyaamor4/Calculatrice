06c5d74be9d1a731da8a8da6d68ec1632613aa63
Slim <slim.kanoun@enis.tn>
2025-11-06 00:45:19 +0100

    Ajout de la soustraction et de l’addition et leurs tests avec a et b comme paramètres et affichage de la valeur de a, de la valeur de b et la valeur de l’opération de calcul effectuée

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+        System.out.println(a + " * " + b + " = " + Operation.multiplication(a, b));
+    }
+}
