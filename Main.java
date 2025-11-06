5e69b73b3267d8cea9afa9aeee42f037cedf1cef
Slim Kanoun <slim.kanoun@enis.tn>
2025-11-06 01:09:12 +0100

    Ajout des méthodes addition et soustraction et mise à jour du Main pour les tester

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int a = 2, b = 3;
+
         System.out.println(a + " * " + b + " = " + Operation.multiplication(a, b));
+        System.out.println(a + " - " + b + " = " + Operation.soustraction(a, b));
+        System.out.println(a + " + " + b + " = " + Operation.addition(a, b));
     }
 }
+
--- Operation.java
+++ Operation.java
@@ -2,4 +2,13 @@ public class Operation {
     public static int multiplication(int x, int y) {
         return x * y;
     }
+
+    public static int soustraction(int x, int y) {
+        return x - y;
+    }
+
+    public static int addition(int x, int y) {
+        return x + y;
+    }
 }
+
